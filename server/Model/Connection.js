487e15c42c74a81baa33a43c2c724ed2967d5775
Pradipta Banerjee <banerjeepradipta08@gmail.com>
2023-08-15 17:19:43 +0530

    adding env

--- server/Model/Connection.js
+++ server/Model/Connection.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Pradipta:Pro08@portfolio.vspu42l.mongodb.net/")
+mongoose.connect(process.env.DBURL)
 .then(()=>{console.log("MongoDB Connected");})
 .catch((err)=>{console.log("DB Error",err);})
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config();
 const projectRoute =require("./Routes/projectRouter");
 const contactRoute =require("./Routes/contactRouter");
 const connection = require("./Model/Connection")
@@ -7,7 +8,6 @@ const app=express()
 app.use(express.json())
 app.use("/api/v1",projectRoute);
 app.use("/api/v1",contactRoute);
-
 
 const PORT=8000;
 app.listen(PORT,()=>{console.log(`Server started at port ${PORT}`);})
